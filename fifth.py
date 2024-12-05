22c0d00d09d769be5994fdd35b72addf5cd04558
KlaraKant <kantok01@jcu.cz>
2024-12-05 17:45:19 +0000

    Pátý úkol

--- fifth.py
+++ fifth.py
@@ -7,13 +7,16 @@ gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
 
-
+#28.11.2024 VRBAT - implementation
 def read_header(file_name, header_length):
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return b'xxx'
+    with open(file_name, "rb") as file_name: 
+        header_length = file_name.read(8) #přečte prvních 8 bajtů z hlavičky souboru [.jpeg = 3 bytes; .gif = 6 bytes; .png = 8 bytes]
+
+    return header_length
 
 
 def is_jpeg(file_name):
@@ -24,19 +27,26 @@ def is_jpeg(file_name):
     # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
 
-    # vyhodnoť zda je soubor jpeg
-
-    return False
-
+    if jpeg_header in header:  #vyhodnoť zda je soubor jpeg
+        return True
+    else:
+        return False
+  
 
 def is_gif(file_name):
     """
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
-    # vyhodnoť zda je soubor gif
-    return False
-
+    #.gif má dva standardy pro zápis formátu v bin
+    header = read_header(file_name, len(gif_header1))
+    if gif_header1 in header: #vyhodnoť zda je soubor gif
+        return True
+    elif gif_header2 in header: #vyhodnoť zda je soubor gif2
+        return True
+    else:
+        return False
+    
 
 def is_png(file_name):
     """
@@ -44,7 +54,11 @@ def is_png(file_name):
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
     # vyhodnoť zda je soubor png
-    return False
+    header = read_header(file_name, len(png_header))
+    if png_header in header: #vyhodnoť zda je soubor png
+        return True
+    else:
+        return False
 
 
 def print_file_type(file_name):
@@ -63,5 +77,12 @@ def print_file_type(file_name):
 
 if __name__ == '__main__':
     # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    try: 
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+
+    except FileNotFoundError:
+        print(f'Soubor {sys.argv[1]} nenalezen.')
+
+    except Exception:
+        print("Byla vyvolána výjimka")
