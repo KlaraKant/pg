d1d315225b40389c5f6bac7aa70e4e0a4c7fdd4a
KlaraKant <kantok01@jcu.cz>
2025-04-19 10:11:07 +0000

    moodie

--- moodie/app.py
+++ moodie/app.py
@@ -42,4 +42,4 @@ def chat():
     return render_template("chat.html", username=username, message=greeting, next_step="mood")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
